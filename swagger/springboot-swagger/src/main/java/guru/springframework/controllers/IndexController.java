8446dc2d6b86e988e766a6d48b7f5b9c35a7ec26
wefine <wefine@aliyun.com>
2017-05-17 22:41:22 +0800

    add a spring boot swagger demo
    Signed-off-by: wefine <wefine@aliyun.com>

---
+++ swagger/springboot-swagger/src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index.html";
+    }
+}
