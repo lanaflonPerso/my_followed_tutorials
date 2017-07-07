7108bfd1f5e8ac98c5147f00267591c571956497
wefine <wefine@aliyun.com>
2017-07-07 16:28:58 +0800

    enable caches.
    Signed-off-by: wefine <wefine@aliyun.com>

--- caches/caches-springboot/src/main/java/com/wefine/AppRunner.java
+++ caches/caches-springboot/src/main/java/com/wefine/AppRunner.java
@@ -20,6 +20,9 @@ public class AppRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         logger.info(".... Fetching books");
         logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
         logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
         logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
     }
